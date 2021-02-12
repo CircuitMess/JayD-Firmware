db97accbf7b782b9aef00b8d648b597989bfef59
hrvojekolar <hrvojekolar1998@gmail.com>
2021-02-12 15:03:41 +0100

    added display features

--- JayD-Firmware.ino
+++ JayD-Firmware.ino
@@ -1,6 +1,27 @@
 #include <Arduino.h>
+#include <CircuitOS.h>
+#include <TFT_eSPI.h>
+#include <Display/Display.h>
+#include <Input/InputGPIO.h>
+#include <Input/Input.h>
+#include <Loop/LoopListener.h>
+#include <Loop/LoopManager.h>
+#include "src/Screens/Playback/Playback.h"
+
+#define blPin 25
+
+Display display(160,128,-1,-1);
+Playback* playback;
 
 void setup(){
+	Serial.begin(115200);
+	pinMode(blPin,OUTPUT);
+	digitalWrite(blPin,LOW);
+	display.begin();
+	playback=new Playback(display);
+	playback->unpack();
+	playback->start();
+
 }
 
 void loop(){
