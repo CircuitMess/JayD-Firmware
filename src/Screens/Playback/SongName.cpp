cf7fc71406e9f73e043b6ee10f08ebf6c1005dff
hrvojekolar <hrvojekolar1998@gmail.com>
2021-02-12 15:04:10 +0100

    initial commit

---
+++ src/Screens/Playback/SongName.cpp
@@ -0,0 +1,17 @@
+#include "SongName.hpp"
+
+SongName::SongName(ElementContainer *parent) : CustomElement(parent,40,20){
+
+
+}
+
+void SongName::draw(){
+
+	getSprite()->setTextFont(1);
+	getSprite()->setTextColor(TFT_WHITE);
+	getSprite()->setTextSize(2);
+	getSprite()->setTextFont(1);
+	getSprite()->setCursor(getTotalX(), getTotalY());
+	getSprite()->printCenter("CAVOGLAVE");
+
+}
---
+++ src/Screens/Playback/SongName.hpp
@@ -0,0 +1,16 @@
+#ifndef JAYD_FIRMWARE_SONGNAME_HPP
+#define JAYD_FIRMWARE_SONGNAME_HPP
+
+#include <UI/CustomElement.h>
+
+class SongName : public CustomElement {
+public:
+	SongName(ElementContainer *parent);
+
+	void draw();
+
+private:
+
+};
+
+#endif //JAYD_FIRMWARE_SONGNAME_HPP
