7c893221f32a7f9589374a4219151960021b8ceb
hrvojekolar <hrvojekolar1998@gmail.com>
2021-02-23 13:48:56 +0100

    added check for currentDuration

--- src/Screens/Playback/TrackCounter.cpp
+++ src/Screens/Playback/TrackCounter.cpp
@@ -12,9 +12,9 @@ void TrackCounter::draw(){
 	getSprite()->setCursor(getTotalX(), getTotalY());
 	getSprite()->fillRoundRect(getTotalX(), getTotalY() + 10, currentDuration, 8, 5, TFT_GREENYELLOW);
 	getSprite()->drawRoundRect(getTotalX(), getTotalY() + 10, 140, 8, 5, TFT_WHITE);
-	getSprite()->printf("%02d:%02d", currentDuration/60, currentDuration - (currentDuration / 60)*60);
+	getSprite()->printf("%02d:%02d", currentDuration / 60, currentDuration - (currentDuration / 60) * 60);
 	getSprite()->setCursor(getTotalX() + 110, getTotalY());
-	getSprite()->printf("%02d:%02d", totalDuration / 60, totalDuration - (totalDuration / 60)*60);
+	getSprite()->printf("%02d:%02d", totalDuration / 60, totalDuration - (totalDuration / 60) * 60);
 
 }
 
@@ -23,7 +23,12 @@ void TrackCounter::setTotalDuration(int totalDuration){
 }
 
 void TrackCounter::setCurrentDuration(int currentDuration){
-	TrackCounter::currentDuration = currentDuration;
+	if(currentDuration < totalDuration){
+		TrackCounter::currentDuration = currentDuration;
+	}else if(currentDuration >= totalDuration){
+		TrackCounter::currentDuration = totalDuration;
+	}
 }
+
 
 
