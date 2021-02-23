b95e6bd4ac058cd5387dd1e9143895cd9c529ad9
hrvojekolar <hrvojekolar1998@gmail.com>
2021-02-23 18:06:43 +0100

    added switching effects

--- src/Screens/MixScreen/Effects.cpp
+++ src/Screens/MixScreen/Effects.cpp
@@ -14,31 +14,37 @@ MixScreen::Effects::Effects(ElementContainer *parent, bool mirrored) : CustomEle
 uint16_t *ikone[] = {lowpass, highpass, speed, reverb, bitcrusher};
 
 void MixScreen::Effects::setEffect(Effect effect){
-	Effects::effect = effect;
+	if(effect > 4 || effect < 0){
+		Effects::effect = static_cast<Effect>(0);
+	}else{
+		Effects::effect = effect;
+	}
 }
 
 void MixScreen::Effects::setIntensity(uint8_t intensity){
-	if(intensity==0){
+	if(intensity == 0){
 		Effects::intensity = 0;
-	}else if(intensity>0 && intensity<255){
+	}else if(intensity > 0 && intensity < 255){
 		Effects::intensity = intensity;
-	}else if(intensity==255){
-		Effects::intensity =255;
+	}else if(intensity == 255){
+		Effects::intensity = 255;
 	}
 }
 
 void MixScreen::Effects::draw(){
 	if(selected){
-		getSprite()->fillRect(getTotalX(), getTotalY() + 25,70,18,TFT_YELLOW);
+		getSprite()->fillRect(getTotalX(), getTotalY() + 25, 77, 18, TFT_YELLOW);
 	}
 	if(!mirrored){
 		getSprite()->drawIcon(ikone[effect], getTotalX(), getTotalY() + 25, 16, 16, 1, TFT_BLACK);
-		getSprite()->fillRoundRect(getTotalX() + 25, getTotalY() + 30, (((float)intensity)/255.0f)*45.0f, 8, 5, TFT_GREENYELLOW);
+		getSprite()->fillRoundRect(getTotalX() + 25, getTotalY() + 30, (((float) intensity) / 255.0f) * 45.0f, 8, 5,
+								   TFT_GREENYELLOW);
 		getSprite()->drawRoundRect(getTotalX() + 25, getTotalY() + 30, 45, 8, 5, TFT_WHITE);
 	}
 	if(mirrored){
 		getSprite()->drawIcon(ikone[effect], getTotalX() + 58, getTotalY() + 25, 16, 16, 1, TFT_BLACK);
-		getSprite()->fillRoundRect(getTotalX() + 2, getTotalY() + 30, (((float)intensity)/255.0f)*45.0f, 8, 5, TFT_GREENYELLOW);
+		getSprite()->fillRoundRect(getTotalX() + 2, getTotalY() + 30, (((float) intensity) / 255.0f) * 45.0f, 8, 5,
+								   TFT_GREENYELLOW);
 		getSprite()->drawRoundRect(getTotalX() + 2, getTotalY() + 30, 45, 8, 5, TFT_WHITE);
 	}
 
