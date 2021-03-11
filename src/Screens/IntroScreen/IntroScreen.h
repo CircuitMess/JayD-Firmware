e8bb1f0d1e8c7f20e2c0c718f94d11067e192633
hrvojekolar <hrvojekolar1998@gmail.com>
2021-03-11 21:33:09 +0100

    added introGIF to intro screen

---
+++ src/Screens/IntroScreen/IntroScreen.h
@@ -0,0 +1,37 @@
+#ifndef JAYD_FIRMWARE_INTROSCREEN_H
+#define JAYD_FIRMWARE_INTROSCREEN_H
+
+#include <Support/Context.h>
+#include <UI/Screen.h>
+#include <UI/LinearLayout.h>
+#include <Loop/LoopListener.h>
+#include <Display/AnimatedSprite.h>
+
+
+namespace IntroScreen {
+
+	class IntroScreen: public Context, public LoopListener {
+	public:
+
+		IntroScreen(Display &display);
+
+		//virtual ~IntroScreen();
+
+		void start();
+
+		void stop();
+
+		void draw();
+
+		void loop(uint micros) override;
+
+	private:
+		static IntroScreen *instance;
+
+		AnimatedSprite gifIntro;
+
+
+	};
+}
+
+#endif //JAYD_FIRMWARE_INTROSCREEN_H
