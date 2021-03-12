b54f24e717fbf77e48bb51d795f58f708c0480b3
hrvojekolar <hrvojekolar1998@gmail.com>
2021-03-12 13:47:35 +0100

    Intro screen (#6)

---
+++ src/Screens/IntroScreen/IntroScreen.cpp
@@ -0,0 +1,50 @@
+#include "IntroScreen.h"
+#include "Bitmaps/introGIF.h"
+#include <FS.h>
+#include <Display/PGMFile.h>
+
+
+IntroScreen::IntroScreen *IntroScreen::IntroScreen::instance = nullptr;
+
+
+IntroScreen::IntroScreen::IntroScreen(Display &display) : Context(display), gifIntro(screen.getSprite(), fs::File(std::make_shared<PGMFile>(intro_gif, sizeof(intro_gif)))){
+
+	instance = this;
+
+	gifIntro.setXY(screen.getTotalX(), screen.getTotalY());
+
+	pack();
+
+}
+
+void IntroScreen::IntroScreen::draw(){
+	screen.getSprite()->clear(TFT_BLACK);
+	screen.draw();
+}
+
+void IntroScreen::IntroScreen::start(){
+
+	draw();
+	screen.commit();
+	gifIntro.setLoopDoneCallback([]{
+		instance->stop();
+		delete instance;
+	});
+
+}
+
+void IntroScreen::IntroScreen::stop(){
+
+}
+
+void IntroScreen::IntroScreen::loop(uint micros){
+
+	if(gifIntro.newFrameReady()){
+		draw();
+		gifIntro.push();
+		screen.commit();
+	}
+
+}
+
+
