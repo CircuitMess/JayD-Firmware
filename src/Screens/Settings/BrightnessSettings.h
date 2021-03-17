51ed9bd457aa34f319ab030cb685651d9c013450
hrvojekolar <hrvojekolar1998@gmail.com>
2021-03-17 14:44:27 +0100

    add brightness bar for matrix light intensity in Settings

--- src/Screens/Settings/BrightnessSettings.h
+++ src/Screens/Settings/BrightnessSettings.h
@@ -2,23 +2,25 @@
 #define JAYD_FIRMWARE_BRIGHTNESSSETTINGS_HPP
 
 #include "SettingsElement.h"
+#include "DropDownSetting.h"
 
 namespace Settings {
 	class BrightnessSetting : public SettingsElement {
 	public:
 		BrightnessSetting(ElementContainer *partent, String name);
 
-		void activated() override;
+		void activate() override;
 
 		void setBightnessValue(uint8_t value);
 
 	private:
+		DropDownSetting *dropDownSetting;
 
 		bool showBrightnessValueBar = false;
 
 		int8_t value;
 
-		int8_t brightnessValue=0;
+		int8_t brightnessValue = 0;
 
 	protected:
 		void drawControl() override;
