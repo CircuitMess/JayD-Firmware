bb6442e297c2adf0d388f612403143f30844abf3
hrvojekolar <hrvojekolar1998@gmail.com>
2021-03-17 14:44:27 +0100

    add brightness bar for matrix light intensity in Settings

--- src/Screens/Settings/BrightnessSettings.cpp
+++ src/Screens/Settings/BrightnessSettings.cpp
@@ -1,40 +1,40 @@
 #include "BrightnessSettings.h"
 
-Settings::BrightnessSetting::BrightnessSetting(ElementContainer *partent, String name): SettingsElement(partent, name){
+Settings::BrightnessSetting::BrightnessSetting(ElementContainer *partent, String name) : SettingsElement(partent, name){
 
 }
 
 void Settings::BrightnessSetting::setBightnessValue(uint8_t value){
-	brightnessValue=brightnessValue+value;
-	if(brightnessValue>100){
-		brightnessValue=100;
-	}else if(brightnessValue<0){
-		brightnessValue=0;
+	brightnessValue = brightnessValue + value;
+	if(brightnessValue > 100){
+		brightnessValue = 100;
+	}else if(brightnessValue < 0){
+		brightnessValue = 0;
 	}
 }
 
-void Settings::BrightnessSetting::activated(){
+void Settings::BrightnessSetting::activate(){
 	showBrightnessValueBar = !showBrightnessValueBar;
 }
 
 void Settings::BrightnessSetting::drawControl(){
-	float movingCursor;
-	getSprite()->setTextFont(1);
-	getSprite()->setTextColor(TFT_WHITE);
-	getSprite()->setTextSize(1);
-	getSprite()->setCursor(getTotalX() + 122, getTotalY() + 8);
-	getSprite()->println(brightnessValue);
-	if(showBrightnessValueBar){
-		if(brightnessValue==0){
-			movingCursor=1;
+		float movingCursor;
+		if(brightnessValue == 0){
+			movingCursor = 0;
 		}else{
-			movingCursor=((float)brightnessValue/100)*54.0f;
+			movingCursor = ((float) brightnessValue / 100) * 51.0f;
 		}
-		getSprite()->drawRect(getTotalX()+100,getTotalY()+20,2,5,TFT_WHITE);
-		getSprite()->drawRect(getTotalX()+155,getTotalY()+20,2,5,TFT_WHITE);
-		getSprite()->drawRect(getTotalX()+100,getTotalY()+22,55,1,TFT_WHITE);
-		getSprite()->drawRect(getTotalX()+100+movingCursor,getTotalY()+20,2,5,TFT_WHITE);
-	}
+		getSprite()->drawRect(getTotalX() + 100, getTotalY() + 10, 2, 5, TFT_WHITE);
+		getSprite()->drawRect(getTotalX() + 153, getTotalY() + 10, 2, 5, TFT_WHITE);
+		getSprite()->drawRect(getTotalX() + 100, getTotalY() + 12, 55, 1, TFT_WHITE);
+		if(showBrightnessValueBar){
+			getSprite()->drawRect(getTotalX() + 101 + movingCursor, getTotalY() + 9, 2, 6, TFT_BLUE);
+		}else{
+			getSprite()->drawRect(getTotalX() + 101 + movingCursor, getTotalY() + 10, 2, 5, TFT_WHITE);
+
+		}
+
 }
+
 
 
