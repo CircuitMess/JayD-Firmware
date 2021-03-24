c598a01c45a4e1c3136f80484d2b0d720a64c3f0
Filip Budiša <filip@circuitmess.com>
2021-03-24 14:01:35 +0100

    Fix SongName include guards

--- src/Screens/MixScreen/SongName.h
+++ src/Screens/MixScreen/SongName.h
@@ -1,18 +1,20 @@
-#ifndef JAYD_FIRMWARE_SONGNAME_H
-#define JAYD_FIRMWARE_SONGNAME_H
+#ifndef JAYD_FIRMWARE_MIX_SONGNAME_H
+#define JAYD_FIRMWARE_MIX_SONGNAME_H
 
 #include <UI/CustomElement.h>
 
 namespace MixScreen {
 	class SongName : public CustomElement {
 	public:
-		SongName(ElementContainer *parent, String songName);
+		SongName(ElementContainer *parent);
 
 		void draw();
 		bool checkScrollUpdate();
 
+		void setSongName(const String& songName);
+
 	private:
-		String songName;
+		String songName = "";
 		bool scrolling = false;
 		const int32_t scrollSpeed = 100; //in milliseconds
 		uint32_t currentTime = 0;
@@ -23,4 +25,4 @@ namespace MixScreen {
 }
 
 
-#endif //JAYD_FIRMWARE_SONGNAME_H
+#endif //JAYD_FIRMWARE_MIX_SONGNAME_H
