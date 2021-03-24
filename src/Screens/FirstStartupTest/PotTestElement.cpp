fcb96c7e60671d14db85eb30867729ece72d130d
hrvojekolar <hrvojekolar1998@gmail.com>
2021-03-24 14:55:23 +0100

    change counter max value in PotTestElement

--- src/Screens/FirstStartupTest/PotTestElement.cpp
+++ src/Screens/FirstStartupTest/PotTestElement.cpp
@@ -7,10 +7,10 @@ PotTestElement::PotTestElement(ElementContainer *parent, bool horizontal) : Cust
 
 void PotTestElement::potMove(uint8_t newValue){
 	value = newValue;
-	if(counter < 100){
+	if(counter < 50){
 		counter++;
 	}else{
-		counter = 100;
+		counter = 50;
 		done = true;
 	}
 }
@@ -18,14 +18,14 @@ void PotTestElement::potMove(uint8_t newValue){
 void PotTestElement::draw(){
 	if(!horizontal){
 		getSprite()->drawRect(getTotalX() - 8, getTotalY() + 2, 2, 55, !done? TFT_DARKGREY:TFT_GREEN);
-		getSprite()->drawRect(getTotalX() - 8, getTotalY() + 2, 2, (((float) counter) / 100.0f) * 55.0f, TFT_GREEN);
+		getSprite()->drawRect(getTotalX() - 8, getTotalY() + 2, 2, (((float) counter) / 50.0f) * 55.0f, TFT_GREEN);
 		getSprite()->drawRect(getTotalX() - 10, getTotalY() + 1, 6, 2, !done? TFT_DARKGREY:TFT_GREEN);
 		getSprite()->drawRect(getTotalX() - 10, getTotalY() + 56, 6, 2, !done? TFT_DARKGREY:TFT_GREEN);
 		getSprite()->drawRect(getTotalX() - 10, getTotalY() + 56 - (((float) value) / 255.0f) * 59.0f, 6, 2, TFT_GREEN);
 	}
 	if(horizontal){
 		getSprite()->drawRect(getTotalX() + 25, getTotalY() + 22, 55, 2, !done? TFT_DARKGREY:TFT_GREEN);
-		getSprite()->drawRect(getTotalX() + 25, getTotalY() + 22, (((float) counter) / 100.0f) * 55.0f, 2, TFT_GREEN);
+		getSprite()->drawRect(getTotalX() + 25, getTotalY() + 22, (((float) counter) / 50.0f) * 55.0f, 2, TFT_GREEN);
 		getSprite()->drawRect(getTotalX() + 25, getTotalY() + 20, 2, 6, !done? TFT_DARKGREY:TFT_GREEN);
 		getSprite()->drawRect(getTotalX() + 80, getTotalY() + 20, 2, 6, !done? TFT_DARKGREY:TFT_GREEN);
 		getSprite()->drawRect(getTotalX() + 25 + (((float) value) / 255.0f) * 60.0f, getTotalY() + 20, 2, 6, TFT_GREEN);
