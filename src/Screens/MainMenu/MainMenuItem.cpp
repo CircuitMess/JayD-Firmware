b4ac0f6b08421132da1e77bd3245bd3d2a308651
hrvojekolar <hrvojekolar1998@gmail.com>
2021-03-25 15:28:42 +0100

    add gifs to SPIFFS in MainMenuItem

--- src/Screens/MainMenu/MainMenuItem.cpp
+++ src/Screens/MainMenu/MainMenuItem.cpp
@@ -6,19 +6,22 @@
 #include "Bitmaps/playbackGIF.hpp"
 #include "Bitmaps/settingsGIF.hpp"
 #include <FS/PGMFile.h>
+#include <SPIFFS.h>
 
 
 uint16_t *icons[] = {playback, dj, settings};
 
-unsigned char *gifIcons[] = {playback_gif, dj_gif, settings_gif};
+String gifIcons[] = {"/playbackGIF.g565", "/dj.g565", "/settingsGIF.g565"};
 
-size_t gifIconsSize[] = {sizeof(playback_gif), sizeof(dj_gif), sizeof(settings_gif)};
+//size_t gifIconsSize[] = {sizeof(playback_gif), sizeof(dj_gif), sizeof(settings_gif)};
 
-MainMenu::MainMenuItem::MainMenuItem(ElementContainer *parent, MenuItemType type) : CustomElement(parent, 20, 20), type(type),
-																					gif(getSprite(), fs::File(std::make_shared<PGMFile>(gifIcons[type], gifIconsSize[type]))){
+MainMenu::MainMenuItem::MainMenuItem(ElementContainer *parent, MenuItemType type) : CustomElement(parent, 20, 20), type(type){
+
+	gif = new AnimatedSprite(getSprite(), SPIFFS.open(gifIcons[type]));
+	gif->setLoop(true);
+
 
 }
-
 
 
 void MainMenu::MainMenuItem::draw(){
@@ -27,10 +30,10 @@ void MainMenu::MainMenuItem::draw(){
 	getSprite()->setTextSize(2);
 
 	if(selected){
-		gif.setXY(getTotalX(), getTotalY() + 40);
-		gif.push();
-	}
-	else{
+		gif->setXY(getTotalX(), getTotalY() + 40);
+		gif->nextFrame();
+		gif->push();
+	}else{
 		getSprite()->drawIcon(icons[type], getTotalX(), getTotalY() + 45, 45, 42, 1, TFT_TRANSPARENT);
 	}
 
@@ -42,10 +45,14 @@ void MainMenu::MainMenuItem::isSelected(bool selected){
 
 bool MainMenu::MainMenuItem::needsUpdate(){
 	if(selected){
-		return gif.checkFrame();
+		return gif->checkFrame();
 	}else{
 		return false;
 	}
+}
+
+MainMenu::MainMenuItem::~MainMenuItem(){
+	delete gif;
 }
 
 
