c5f7bb6110461f4b40891468ca87fdca7c0d28f8
Emil Gajšak <emil.gajsak@gmail.com>
2021-03-28 10:50:16 +0200

    Changed fonts in mixScreen and playbackScreen

--- src/Screens/Playback/SongName.cpp
+++ src/Screens/Playback/SongName.cpp
@@ -1,19 +1,17 @@
 #include "SongName.h"
 
-Playback::SongName::SongName(ElementContainer *parent) : CustomElement(parent,40,20){
+Playback::SongName::SongName(ElementContainer *parent) : CustomElement(parent,160,20){
 
 
 }
 
 void Playback::SongName::draw(){
-
-	getSprite()->setTextFont(1);
-	getSprite()->setTextColor(TFT_WHITE);
-	getSprite()->setTextSize(1);
-	getSprite()->setTextFont(2);
-	getSprite()->setCursor(getTotalX(), getTotalY() + 10);
-	getSprite()->printCenter(songName);
-
+	FontWriter u8f = getSprite()->startU8g2Fonts();
+	u8f.setFont(u8g2_font_DigitalDisco_tf);
+	u8f.setFontMode(1);
+	u8f.setForegroundColor(TFT_WHITE);
+	u8f.setCursor(getTotalX() + ((160 - u8f.getUTF8Width(songName.c_str())) / 2) , getTotalY() + 20);
+	u8f.print(songName);
 }
 
 void Playback::SongName::setSongName(const String &songName){
