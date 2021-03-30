ec861e92bfae844d33de466138f94e5a7c16b616
hrvojekolar <hrvojekolar1998@gmail.com>
2021-03-30 14:24:16 +0200

    Add MainMenu gifs (#18)
    * add gifs to SPIFFS in MainMenuItem
    
    * change gif type to pointer
    
    * put gifs to SPIFFS and add compressedFile in MainMenu
    
    * put gif set masking color to constructor
    
    * add starting value nullptr to "buffer" in MainMenu.h
    
    * add check if "buffer" is empty in MainMenu.cpp
    
    * add background.close() in ~MainMenu

--- src/Screens/MainMenu/MainMenuItem.cpp
+++ src/Screens/MainMenu/MainMenuItem.cpp
@@ -6,19 +6,22 @@
 #include "Bitmaps/playbackGIF.hpp"
 #include "Bitmaps/settingsGIF.hpp"
 #include <FS/PGMFile.h>
+#include <SPIFFS.h>
 
 
-uint16_t *icons[] = {playback, dj, settings};
+uint16_t *icons[] = {/*playback,*/ dj, settings};
 
-unsigned char *gifIcons[] = {playback_gif, dj_gif, settings_gif};
+String gifIcons[] = {/*"/playbackGIF.g565",*/"/dj.g565", "/settingsGIF.g565"};
 
-size_t gifIconsSize[] = {sizeof(playback_gif), sizeof(dj_gif), sizeof(settings_gif)};
+//size_t gifIconsSize[] = {sizeof(playback_gif), sizeof(dj_gif), sizeof(settings_gif)};
 
-MainMenu::MainMenuItem::MainMenuItem(ElementContainer *parent, MenuItemType type) : CustomElement(parent, 20, 20), type(type),
-																					gif(getSprite(), fs::File(std::make_shared<PGMFile>(gifIcons[type], gifIconsSize[type]))){
+MainMenu::MainMenuItem::MainMenuItem(ElementContainer *parent, MenuItemType type) : CustomElement(parent, 20, 20), type(type){
+
+	gif = new AnimatedSprite(getSprite(), SPIFFS.open(gifIcons[type]));
+	gif->setLoop(true);
+	gif->setMaskingColor(TFT_BLACK);
 
 }
-
 
 
 void MainMenu::MainMenuItem::draw(){
@@ -27,11 +30,11 @@ void MainMenu::MainMenuItem::draw(){
 	getSprite()->setTextSize(2);
 
 	if(selected){
-		gif.setXY(getTotalX(), getTotalY() + 40);
-		gif.push();
-	}
-	else{
-		getSprite()->drawIcon(icons[type], getTotalX(), getTotalY() + 45, 45, 42, 1, TFT_TRANSPARENT);
+		gif->setXY(getTotalX(), getTotalY() + 40);
+		gif->nextFrame();
+		gif->push();
+	}else{
+		getSprite()->drawIcon(icons[type], getTotalX(), getTotalY() + 45, 45, 42, 1, TFT_BLACK);
 	}
 
 }
@@ -42,10 +45,14 @@ void MainMenu::MainMenuItem::isSelected(bool selected){
 
 bool MainMenu::MainMenuItem::needsUpdate(){
 	if(selected){
-		return gif.checkFrame();
+		return gif->checkFrame();
 	}else{
 		return false;
 	}
+}
+
+MainMenu::MainMenuItem::~MainMenuItem(){
+	delete gif;
 }
 
 
